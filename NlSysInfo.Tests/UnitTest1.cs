3632f90f0af2fbcf793dc8afcdb23524e29f2afc
raddevus <1radeutsch1@gmail.com>
2024-11-15 16:59:16 -0500

    Beginning unit tests -- examining what can be done.

--- NlSysInfo.Tests/UnitTest1.cs
+++ NlSysInfo.Tests/UnitTest1.cs
@@ -1,10 +1,16 @@
+using NewLibre;
 namespace NlSysInfo.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestProcNames()
     {
-
+        SystemInfo si = new();
+        List<string> allNames = si.GetAllProcNames();
+        foreach (string s in allNames){
+            Console.Write($"{s}, ");
+        }
+        // Assert.Contains("teams:5275",allNames);
     }
 }
