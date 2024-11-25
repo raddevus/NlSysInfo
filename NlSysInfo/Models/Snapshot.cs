844d89f23848372c8c25d88cb7f9884eac38e4dd
raddevus <1radeutsch1@gmail.com>
2024-11-25 16:59:09 -0500

    Added file but forgot to include in prev commit

---
+++ NlSysInfo/Models/Snapshot.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+
+namespace NewLibre;
+public class Snapshot{
+    public Int32 Id{get;set;}
+    public string Name{get;set;}
+    public string Filename{get;set;}
+
+    public string? Category{get;set;}
+    
+    public string FileHash{get;set;}
+
+    public long FileSize{get;set;}
+    
+    public String? Created{get;set;}
+
+    public Snapshot(String name, String filename)
+    {
+        name = name.Trim();
+        if (!String.IsNullOrEmpty(name)){
+            var endIdx = name.IndexOf(' ');
+            if (endIdx > 0){
+                name = name.Substring(0,endIdx);
+            }
+        }
+        Name = name;
+        Filename = filename;
+        Created = DateTime.Now.ToString();
+        try{
+            if (Filename != null && Filename != String.Empty){
+                FileInfo fi = new FileInfo(Filename);
+                FileSize = fi.Length;
+            }
+        }
+        catch (Exception ex){
+            Console.WriteLine($"FAIL! : {ex.GetType().ToString()}:{ex.Message}");
+            throw ex;
+        }
+    }
+}
