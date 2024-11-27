19910046f295054b1d21ade756c9b8bc88590aa2
raddevus <1radeutsch1@gmail.com>
2024-11-27 09:51:44 -0500

    getting count for max created date

--- NlSysInfo/DevNotes/helpfulQueries.sql
+++ NlSysInfo/DevNotes/helpfulQueries.sql
@@ -1,2 +1,5 @@
 -- select the max created - 1 (so you can compare to max created)
 select created, count(*) from snapshot where created = (select max(created) from snapshot where created <  (select max(created) from snapshot));
+
+-- select max created
+select created, count(*) from snapshot where created = (select max(created)from snapshot);
