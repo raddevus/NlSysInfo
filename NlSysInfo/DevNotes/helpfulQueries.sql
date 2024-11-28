c016305c6928621a1c231940b767c7257aae27d7
raddevus <1radeutsch1@gmail.com>
2024-11-27 23:20:06 -0500

    adding queries used for comparison of snapshots

--- NlSysInfo/DevNotes/helpfulQueries.sql
+++ NlSysInfo/DevNotes/helpfulQueries.sql
@@ -3,3 +3,10 @@ select created, count(*) from snapshot where created = (select max(created) from
 
 -- select max created
 select created, count(*) from snapshot where created = (select max(created)from snapshot);
+
+WITH CompareProcs(name,file) AS 
+(
+    SELECT name, filehash from snapshot where 
+    created = (select max(created) from snapshot)
+    ) select name, filehash from snapshot where created = '11/27/2024 10:22:05 AM' 
+    and name not in (select name from CompareProcs);
