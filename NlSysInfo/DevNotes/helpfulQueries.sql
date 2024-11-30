de376acce72a5054f6a4ac352f52b2e8a69ff6a3
raddevus <1radeutsch1@gmail.com>
2024-11-30 09:15:01 -0500

    Adding queries to help compare snapshot data

--- NlSysInfo/DevNotes/helpfulQueries.sql
+++ NlSysInfo/DevNotes/helpfulQueries.sql
@@ -10,3 +10,16 @@ WITH CompareProcs(name,file) AS
     created = (select max(created) from snapshot)
     ) select name, filehash from snapshot where created = '11/27/2024 10:22:05 AM' 
     and name not in (select name from CompareProcs);
+
+    WITH CompareProcs(name,file) AS 
+(
+    SELECT name, filehash from snapshot where 
+    created = (select max(created) from snapshot)
+    ) select name, filehash from snapshot where created = '11/26/2024 9:59:51 PM' 
+    and name not in (select name from CompareProcs);
+
+    select name, count(name) from snapshot 
+    where created = (select max(created) from snapshot)
+     group by name having count(name) > 1;
+
+     
