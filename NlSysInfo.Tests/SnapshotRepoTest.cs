98d41aaecf5e9a2daa67274441077bed5bbe6d46
raddevus <1radeutsch1@gmail.com>
2024-12-06 16:46:17 -0500

    Altered code to get procinfo data from db via proc name.  Added tests for the snapshotrepository

--- NlSysInfo.Tests/SnapshotRepoTest.cs
+++ NlSysInfo.Tests/SnapshotRepoTest.cs
@@ -8,8 +8,20 @@ public class SnapshotRepoTest{
     [Fact]
     public void GetProcInfo(){
         SnapshotRepository sr = new SnapshotRepository();
-        ProcInfo? pi = JsonSerializer.Deserialize<ProcInfo>(sr.GetMatchingSnapshots());
-        Console.WriteLine($"{pi.Created} {pi.Name} {pi.Filename}");
-        
+        List<ProcInfo> pi = JsonSerializer.Deserialize<List<ProcInfo>>(sr.GetMatchingSnapshotsByName("bash"));
+        // print first and last 
+        int lastIdx = pi.Count-1;
+        if (lastIdx >= 0){
+            Console.WriteLine($"{pi[0].Created} {pi[0].Name} {pi[0].Filename} {pi[0].FileDate ?? "empty string"}");
+            Console.WriteLine($"{pi[lastIdx].Created} {pi[lastIdx].Name} {pi[lastIdx].Filename} {pi[lastIdx].FileDate ?? "empty string"}");
+        }
+    }
+
+    [Fact]
+    public  void GetProcInfoWithBadName(){
+        SnapshotRepository sr = new SnapshotRepository();
+        List<ProcInfo> pi = JsonSerializer.Deserialize<List<ProcInfo>>(sr.GetMatchingSnapshotsByName("fake-proc-name"));
+                
+        Assert.Equal(0,pi.Count);
     }
 }
--- NlSysInfo/Repositories/SnapshotRepository.cs
+++ NlSysInfo/Repositories/SnapshotRepository.cs
@@ -1,15 +1,25 @@
 namespace NewLibre;
+
+using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
 public class SnapshotRepository{
 
-    public string GetMatchingSnapshots(){
+    public string GetMatchingSnapshotsByName(string name){
         SnapshotContext sc = new SnapshotContext();
-        // Snapshot ss = sc.Find<Snapshot>();
+        var allItems = sc.Snapshot.Where<Snapshot>(s => s.Name == name).OrderBy<Snapshot, string>(a => a.Created ).ToList<Snapshot>();
+        
+        Console.WriteLine($"{allItems}");
+        Console.WriteLine($"{allItems.Count()}");
+        
+        foreach (Snapshot i in allItems){
+            Console.WriteLine($"{i.Created}, {i.FileHash}");
+        }
+        //Snapshot ss = sc.Set<Snapshot>().Where<Snapshot>();
         Snapshot ss = new Snapshot("test","/usr/bin/bash");
-        var jsonObject = JsonSerializer.Serialize(ss);
+        var jsonObject = JsonSerializer.Serialize(allItems);
         Console.WriteLine(jsonObject);
         return jsonObject;
     }
