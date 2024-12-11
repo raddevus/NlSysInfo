8e1c2232fed083d4183f988ab5adca6bff23c840
raddevus <1radeutsch1@gmail.com>
2024-12-11 10:23:20 -0500

    Added comment to helpful sql

--- NlSysInfo/DevNotes/helpfulQueries.sql
+++ NlSysInfo/DevNotes/helpfulQueries.sql
@@ -8,7 +8,7 @@ WITH CompareProcs(name,file) AS
 (
     SELECT name, filehash from snapshot where 
     created = (select max(created) from snapshot)
-    ) select name, filehash from snapshot where created = '11/27/2024 10:22:05 AM' 
+    ) select name, filehash from snapshot where created = '2024-12-11 09:25:40' 
     and name not in (select name from CompareProcs);
 
     WITH CompareProcs(name,file) AS 
@@ -18,6 +18,7 @@ WITH CompareProcs(name,file) AS
     ) select name, filehash from snapshot where created = '11/26/2024 9:59:51 PM' 
     and name not in (select name from CompareProcs);
 
+-- checks for duplicates on the max created entries
     select name, count(name) from snapshot 
     where created = (select max(created) from snapshot)
      group by name having count(name) > 1;
