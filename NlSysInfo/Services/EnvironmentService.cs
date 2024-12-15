380a3827a10d75e9e8346dc70cbef57d8fc67f84
raddevus <1radeutsch1@gmail.com>
2024-12-15 17:39:55 -0500

    Changed the List of tuple to list of object so I can return proper json with named values.  Tuple can't be serialized at this time so have to do it this way.

--- NlSysInfo/Services/EnvironmentService.cs
+++ NlSysInfo/Services/EnvironmentService.cs
@@ -5,14 +5,14 @@ public class EnvironmentService{
 
     public String GetSpecialFolders(){
         Environment.SpecialFolder[] allSpecialFolders = (Environment.SpecialFolder[])Enum.GetValues(typeof(Environment.SpecialFolder));
-        List<(string Key, string Value)> specialFoldersOut = new();
+        List<object> specialFoldersOut = new();
 
         foreach (var folder in allSpecialFolders){
             var folderPath = Environment.GetFolderPath(folder);
             if (!String.IsNullOrEmpty(folderPath)){
                 var key = Enum.GetName(typeof(Environment.SpecialFolder), folder);
-                specialFoldersOut.Add((key, folderPath));
-                //Console.WriteLine($"{Enum.GetName(typeof(Environment.SpecialFolder), folder)} path: {folderPath} ");
+                Console.WriteLine($"key: {key}, folderPath: {folderPath}");
+                specialFoldersOut.Add(new {folderName=key, folderPath=folderPath});
             }
         }
         return JsonSerializer.Serialize(specialFoldersOut);
